2ccdf9fe57b3123e7e73c84bdadeb23689f4433e
Jliv316 <jclivingston316@gmail.com>
2018-07-15 16:40:02 -0600

    shot spec

--- spec/features/registered_user_can_take_a_shot_spec.rb
+++ spec/features/registered_user_can_take_a_shot_spec.rb
@@ -5,7 +5,7 @@ describe 'Registered user', type: :request do
     @user1 = create(:user) 
     @user2 = create(:user, api_key: User.generate_api_key)
     @game = create(:game, player_1_id: @user1, player_2_id: @user2, player_1_api_key: @user1.api_key, player_2_api_key: @user2.api_key)
-    BoardService.create_board(@user1, @game, 4)
+
     BoardService.create_board(@user2, @game, 4)
     @board1 = Board.all.first
     @board2 = Board.all.last
@@ -15,20 +15,20 @@ describe 'Registered user', type: :request do
       it 'updates the message and board with a hit' do
 
         @ship1 = create(:ship)
-        @ship2 = create(:ship)
+        @ship2 = create(:ship, length: 3)
         @ship3 = create(:ship)
-        @ship4 = create(:ship)
+        @ship4 = create(:ship, length: 3)
         #player 1 places ships
         SpaceService.occupy!(@board1.spaces[0], @ship1)
         SpaceService.occupy!(@board1.spaces[1], @ship1)
-
+        binding.pry
         SpaceService.occupy!(@board1.spaces[4], @ship2)
         SpaceService.occupy!(@board1.spaces[5], @ship2)
         SpaceService.occupy!(@board1.spaces[6], @ship2)
         #player2 places ships
         SpaceService.occupy!(@board2.spaces[1], @ship3)
         SpaceService.occupy!(@board2.spaces[0], @ship3)
-
+        binding.pry
         SpaceService.occupy!(@board2.spaces[4], @ship4)
         SpaceService.occupy!(@board2.spaces[5], @ship4)
         SpaceService.occupy!(@board2.spaces[6], @ship4)
@@ -40,7 +40,7 @@ describe 'Registered user', type: :request do
         post endpoint, params: payload, headers: {"HTTP_X_API_KEY" => @user2.api_key}
 
         game_data = JSON.parse(response.body)
-
+        binding.pry
         expect(game_data["message"]).to eq("Your shot resulted in a Hit.")
         expect(@board1.ships.first.damage).to eq(1)
       end
