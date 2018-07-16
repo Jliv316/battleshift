e23a6b2181805291fabda1b56dfe1848e2f16266
Jliv316 <jclivingston316@gmail.com>
2018-07-15 18:25:21 -0600

    board service test

--- spec/services/board_service_spec.rb
+++ spec/services/board_service_spec.rb
@@ -6,6 +6,13 @@ describe BoardService do
   let(:game) { create(:game, player_1_id: user1, player_2_id: user2, player_1_api_key: user1.api_key, player_2_api_key: user2.api_key) }
   it "exists when provided a user, game and length" do
     board = BoardService.create_board(user1, game, 4)
-    expect(board).to be_a Board
+    board_array = ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2", "C3", "C4", "D1", "D2", "D3", "D4"]
+    expect(board).to eq(board_array)
+  end
+
+  it 'gets row letters' do
+    board = BoardService.create_board(user1, game, 4)
+    row_letters = BoardService.get_row_letters
+    expect(row_letters).to eq()
   end
 end
