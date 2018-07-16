27c30c594ee0d141940e5287ab25bd071a990a77
Jliv316 <jclivingston316@gmail.com>
2018-07-15 20:11:32 -0600

    adds notification controller spec

--- spec/controllers/notification_controller_spec.rb
+++ spec/controllers/notification_controller_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe NotificationController, type: :controller do
 
   describe "GET #create" do
     it "returns a success response" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
       post :create
 
       expect(flash[:notice]).to be_present
-      expect(result).to redirect_to("/dashboard/#{user1.id}")
+      expect(response.code).to eq("302")
+      expect(response).to redirect_to("/dashboard/#{user1.id}")
     end
   end
 end
