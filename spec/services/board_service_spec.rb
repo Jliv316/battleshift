5bf7e13dd298b1421fc335a5e1709ec3aa1aa99e
Jliv316 <jclivingston316@gmail.com>
2018-07-15 18:20:08 -0600

    board service test

--- spec/services/board_service_spec.rb
+++ spec/services/board_service_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 describe BoardService do
-  @user1 = create(:user) 
-  @user2 = create(:user, api_key: User.generate_api_key)
-  @game = create(:game, player_1_id: @user1, player_2_id: @user2, player_1_api_key: @user1.api_key, player_2_api_key: @user2.api_key)
-  board = BoardService.create_board(@user1, @game, 4)
-
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user, api_key: User.generate_api_key) }
+  let(:game) { create(:game, player_1_id: user1, player_2_id: user2, player_1_api_key: user1.api_key, player_2_api_key: user2.api_key) }
   it "exists when provided a user, game and length" do
+    board = BoardService.create_board(user1, game, 4)
     expect(board).to be_a Board
   end
 end
