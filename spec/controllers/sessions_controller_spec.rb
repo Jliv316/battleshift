aa916efd7eed1b5c34009b851c54b7abcd997dc2
Jliv316 <jclivingston316@gmail.com>
2018-07-15 19:28:23 -0600

    sessions controller test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,40 +1,24 @@
 require 'rails_helper'
 
-describe "GET #index" do
+
+RSpec.describe SessionsController, type: :controller do
+describe "GET login" do
     it "returns a success response" do
-      user = User.create! valid_attributes
-      get :index, params: {}, session: valid_session
-      expect(response).to be_success
+      email = 'jliv316@gmail.com'
+      password = 'password'
+
+      visit '/login'
+
+      expect(current_path).to eq('/login')
+      
+      fill_in :email, with: email
+      fill_in :password, with: password
+
+      click_button 'Log In'
+
+      expect(current_path).to eq(dashboard_path)
     end
   end
+end
 
-  describe "GET #show" do
-    it "returns a success response" do
-      user = User.create! valid_attributes
-      get :show, params: {id: user.to_param}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns a success response" do
-      user = User.create! valid_attributes
-      get :edit, params: {id: user.to_param}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new User" do
-        expect {
-          post :create, params: {user: valid_attributes}, session: valid_session
-        }.to change(User, :count).by(1)
-      end
+  
